95c7fe86d75a394af152c81c102509952c7210e1
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-03-10 14:24:49 +0000

    i updated the uml (plat class)

--- database/migrations/2026_03_10_141429_create_plats_table.php
+++ database/migrations/2026_03_10_141429_create_plats_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
-            $table->integer('stock')->default(0);
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
