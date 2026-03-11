8a0aa346364390c7b6120ac84c451147db2ac585
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-03-11 12:10:18 +0000

    categories crezation

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -18,4 +19,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::apiResource('categories', CategoryController::class);
 });
