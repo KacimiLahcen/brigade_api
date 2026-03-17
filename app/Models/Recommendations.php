84717d2cb5850c5fd73fbe6f5fcf502656db5b63
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-03-17 23:19:25 +0000

    update recommendations  model

--- app/Models/Recommendations.php
+++ app/Models/Recommendations.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Recommendations extends Model
 {
     protected $fillable = ['user_id', 'plate_id', 'score', 'label', 'status', 'warning_message'];
-    
+
+    public function user() {
+    return $this->belongsTo(User::class);
+}
+
 }
