f6e3873a5344d6a9e51b313992ab289b4d83c578
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-03-18 22:43:19 +0000

    update routes

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,22 @@ Route::post('/login', [AuthController::class, 'login']);
 
 //routes need token 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::apiResource('categories', CategoryController::class);
+
+    // Route::apiResource('categories', CategoryController::class);
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::get('/categories/{id}/plates', [CategoryController::class, 'getPlates']);
+
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    
+    
     Route::apiResource('plats', PlatController::class);
 });
